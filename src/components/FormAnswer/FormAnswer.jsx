f12413fc2180581f2c8719e642a85abaf6cf9f35
mdtthanh <maithanh0131@gmail.com>
2023-02-13 13:14:07 +0700

    add

--- src/components/FormAnswer/FormAnswer.jsx
+++ src/components/FormAnswer/FormAnswer.jsx
@@ -1,34 +1,88 @@
-import React from "react";
+import React, {useState} from "react";
 import "./FormAnswer.css";
 import Bookmark from "../Bookmark/Bookmark";
 import Icon from "../Icon/Icon";
 import Avatar from "../Avatar/Avatar";
+import {token} from "../../service/auth";
+import {useParams} from "react-router-dom";
+import {toast} from "react-toastify";
+
 const FormAnswer = (props) => {
-  const { fullName, userName, datetime, comment } = props;
-  return (
-    <div>
-      <div className="faq-comment-header">
-        BÌNH LUẬN
-        <hr className="faq-comment__underline"></hr>
-      </div>
-      <div className="faq-input-border">
-        <div className="faq-input-border__in">
-          {/* {props.children} */}
-          <div className="faq-post__avatar col-span-1">
-            <Avatar />
-          </div>
-          <textarea
-            className="faq-input-comment"
-            name="contents"
-            id=""
-            placeholder="Viết bình luận....."
-            rows="5"
-          ></textarea>
+    const {fullName, userName, datetime, comment} = props;
+    const [commentAction, setCommentAction] = useState("")
+    // const [comments, setComments] = useState([])
+    // const onClickHandle = (e) => {
+    //     setComments((comments) => [...comments, commentAction])
+    // }
+    let {id} = useParams();
+
+    const handleSubmit = (e) => {
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer " + token);
+        myHeaders.append("Content-Type", "application/json");
+        e.preventDefault();
+        var raw = JSON.stringify({
+            "post_id": {id},
+            "content": {commentAction},
+            "parent_id": null
+        });
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+        console.log(raw)
+
+        fetch("https://hedspi.dev/comment/create", requestOptions)
+            .then(response => {
+                return response.json()
+            })
+            .then(result => {
+                if(result.post_id) {
+                    toast.success("Create comment successfull!")
+                    console.log(result)
+                } else {
+                    toast.error("Create comment failed!")
+                }
+                console.log(result)
+            })
+            .catch(error => console.log('error', error));
+    }
+
+    const onChangeHandle = (e) => {
+        setCommentAction(e.target.value)
+    }
+    return (
+        <div>
+            <div className="faq-comment-header">
+                BÌNH LUẬN
+                <hr className="faq-comment__underline"></hr>
+            </div>
+            <form action="" onSubmit={handleSubmit}>
+                <div className="faq-input-border">
+                    <div className="faq-input-border__in">
+                        {/* {props.children} */}
+                        <div className="faq-post__avatar col-span-1">
+                            <Avatar/>
+                        </div>
+                        <textarea
+                            className="faq-input-comment"
+                            name="contents"
+                            id=""
+                            placeholder="Viết bình luận....."
+                            rows="5"
+                            value={commentAction}
+                            onChange={onChangeHandle}
+                        ></textarea>
+                    </div>
+                    <button className="faq-button-comment">Bình luận</button>
+                </div>
+            </form>
+
         </div>
-        <button className="faq-button-comment">Bình luận</button>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default FormAnswer;
--- src/pages/BlogDetail/BlogDetail.jsx
+++ src/pages/BlogDetail/BlogDetail.jsx
@@ -7,145 +7,148 @@ import Heading from "../../components/Heading/Heading";
 import "./BlogDetail.css";
 import Comment from "../../components/Comment/Comment";
 import SimilarPost from "../../components/SimilarPost/SimilarPost";
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import getBlog from "./../../service/blog";
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 
 const BlogDetail = () => {
-  const [name, setName] = useState("");
-  const [createTime, setCreateTime] = useState("");
-  const [view, setView] = useState("");
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [comment, setComment] = useState("");
-  let { id } = useParams();
-  console.log(id)
-  useEffect(() => {
-    (async () => {
-      const profile = await getBlog(id);
-      //   console.log(profile.user)
-      setName(profile.data.name);
-      setTitle(profile.data.title);
-      setContent(profile.data.content);
-      setComment(profile.data.comment);
-    })();
-  }, []);
-  return (
-    <div className="faq-blog ">
-      <div className="faq-corner ">
-        <PostAuthor
-          fullName={name}
-          userName=""
-          datetime="08/12/2022, 00:34 AM"
-          followers="20"
-          questions="10"
-          posts="10"
-          views="10"
-          bookmark="15"
-          comments="10"
-        />
-        <PostScript
-          title={title}
-          tags={["kinh nghiệm", "chia sẻ", "hiragana", "kanji"]}
-          description={content}
-        />
-        <div className="faq-bookmark">
-          <Bookmark content="BOOKMARK" />
-          {/* <TableOfContents>
+    const [name, setName] = useState("");
+    const [createTime, setCreateTime] = useState("");
+    const [view, setView] = useState("");
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [comment, setComment] = useState([]);
+    const [count_comment, setCount_Comment] = useState([]);
+    const [count_view, setCount_View] = useState([]);
+
+    let {id} = useParams();
+    console.log(id)
+    useEffect(() => {
+        (async () => {
+            const profile = await getBlog(id);
+            //   console.log(profile.user)
+            setName(profile.data.name);
+            setTitle(profile.data.title);
+            setContent(profile.data.content);
+            setComment(profile.data.comments.parent_comments);
+            setCreateTime(profile.data.created_at)
+            setCount_Comment(profile.data.comments.total)
+            setCount_View(profile.data.views)
+        })();
+    }, []);
+    return (
+        <div className="faq-blog ">
+            <div className="faq-corner ">
+                <PostAuthor
+                    fullName={name}
+                    userName=""
+                    datetime={createTime}
+                    followers="20"
+                    questions="10"
+                    posts="10"
+                    views={count_view}
+                    bookmark="15"
+                    comments={count_comment}
+                />
+                <PostScript
+                    title={title}
+                    tags={["kinh nghiệm", "chia sẻ", "hiragana", "kanji"]}
+                    description={content}
+                />
+                <div className="faq-bookmark">
+                    <Bookmark content="BOOKMARK"/>
+                    {/* <TableOfContents>
           <Heading
             title="MỤC LỤC"
             size="medium"
             color="var(--color-blue-secondary--)"
           />
         </TableOfContents> */}
-        </div>
-        <div></div>
-      </div>
+                </div>
+                <div></div>
+            </div>
 
-      <div className="col-span-12">
-        <div className="faq-post">
-          <p className="faq-post-title">Bài viết liên quan</p>
-          <div className="faq-post-suggest">
-            <SimilarPost
-              title="Phương pháp luyện nói tiếng Nhật"
-              name="Mai Đào Tuấn Thành"
-              views="10"
-              bookmarks="21"
-              posts="3"
-            />
-            <SimilarPost
-              title="Phương pháp luyện nói tiếng Nhật"
-              name="Mai Đào Tuấn Thành"
-              views="10"
-              bookmarks="21"
-              posts="3"
-            />
-            <SimilarPost
-              title="Phương pháp luyện nói tiếng Nhật"
-              name="Mai Đào Tuấn Thành"
-              views="10"
-              bookmarks="21"
-              posts="3"
-            />
-            <SimilarPost
-              title="Phương pháp luyện nói tiếng Nhật"
-              name="Mai Đào Tuấn Thành"
-              views="10"
-              bookmarks="21"
-              posts="3"
-            />
-          </div>
-        </div>
+            <div className="col-span-12" style={{padding: "0 10%"}}>
+                <div className="faq-post">
+                    <p className="faq-post-title">Bài viết liên quan</p>
+                    <div className="faq-post-suggest">
+                        <SimilarPost
+                            title="Phương pháp luyện nói tiếng Nhật"
+                            name="Mai Đào Tuấn Thành"
+                            views="10"
+                            bookmarks="21"
+                            posts="3"
+                        />
+                        <SimilarPost
+                            title="Phương pháp luyện nói tiếng Nhật"
+                            name="Mai Đào Tuấn Thành"
+                            views="10"
+                            bookmarks="21"
+                            posts="3"
+                        />
+                        <SimilarPost
+                            title="Phương pháp luyện nói tiếng Nhật"
+                            name="Mai Đào Tuấn Thành"
+                            views="10"
+                            bookmarks="21"
+                            posts="3"
+                        />
+                        <SimilarPost
+                            title="Phương pháp luyện nói tiếng Nhật"
+                            name="Mai Đào Tuấn Thành"
+                            views="10"
+                            bookmarks="21"
+                            posts="3"
+                        />
+                    </div>
+                </div>
 
-        <div className="faq-post">
-          <p className="faq-post-title">Bài viết khác từ Mai Đào Tuấn Thành</p>
-          <div className="faq-post-suggest">
-            <SimilarPost
-              title="Phương pháp luyện nói tiếng Nhật"
-              name="Mai Đào Tuấn Thành"
-              views="10"
-              bookmarks="21"
-              posts="3"
-            />
-            <SimilarPost
-              title="Phương pháp luyện nói tiếng Nhật"
-              name="Mai Đào Tuấn Thành"
-              views="10"
-              bookmarks="21"
-              posts="3"
-            />
-            <SimilarPost
-              title="Phương pháp luyện nói tiếng Nhật"
-              name="Mai Đào Tuấn Thành"
-              views="10"
-              bookmarks="21"
-              posts="3"
-            />
-            <SimilarPost
-              title="Phương pháp luyện nói tiếng Nhật"
-              name="Mai Đào Tuấn Thành"
-              views="10"
-              bookmarks="21"
-              posts="3"
-            />
-          </div>
+                <div className="faq-post">
+                    <p className="faq-post-title">Bài viết khác từ {name}</p>
+                    <div className="faq-post-suggest">
+                        <SimilarPost
+                            title="Phương pháp luyện nói tiếng Nhật"
+                            name={name}
+                            views="10"
+                            bookmarks="21"
+                            posts="3"
+                        />
+                        <SimilarPost
+                            title="Phương pháp luyện nói tiếng Nhật"
+                            name={name}
+                            views="10"
+                            bookmarks="21"
+                            posts="3"
+                        />
+                        <SimilarPost
+                            title="Phương pháp luyện nói tiếng Nhật"
+                            name={name}
+                            views="10"
+                            bookmarks="21"
+                            posts="3"
+                        />
+                        <SimilarPost
+                            title="Phương pháp luyện nói tiếng Nhật"
+                            name={name}
+                            views="10"
+                            bookmarks="21"
+                            posts="3"
+                        />
+                    </div>
+                </div>
+                <FormAnswer/>
+                {comment && comment.map((c, i) => (
+                    <Comment
+                        datetime={c.updated_at}
+                        comment={c.content}
+                        fullName="Mai Dao Tuan Thanh"
+                    />
+                ))
+                }
+
+            </div>
         </div>
-        <FormAnswer />
-        <Comment
-          datetime="11/11/2022, 22:20 PM"
-          comment="Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip"
-          fullName="Mai Dao Tuan Thanh"
-          userName="thanhdao"
-        />
-        <Comment
-          datetime="11/11/2022, 22:20 PM"
-          comment="Bài viết rất hay."
-          fullName="Hoang Anh Tuan"
-          userName="tuanha"
-        />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default BlogDetail;
